80caf5a827bb1197dc1ed976b357ead7a0f6fa27
Ducky3264 <49353182+Ducky3264@users.noreply.github.com>
2020-04-28 10:03:48 -0500

    4/28/20, fixed megasize and asking the user if they want a sandwhich.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 class Funcs:
     def AskMenu(self):
         br = True
-        in1, in2, in3, in4, in5 = "", "", "", "", ""
+        in1, in2, in3, in4, in5, in7 = "", "", "", "", "", ""
         price, priceB, fryprice, P_ket = 0.00, 0.00, 0.00, 0.00
         sandoptions = {
             "chicken": 5.25,
@@ -18,19 +18,20 @@ class Funcs:
             "medium": 1.50,
             "large": 2.00
         }
-
-
-        while br:
-            print("Your sandwhich options are,")
-            print(list(sandoptions.keys()))
-            in1 = input("What type of sandwhich would you like?")
-            in1 = in1.lower()
-            try:
-                price = sandoptions[in1]
-                br = False
-            except KeyError:
-                print("That is not an option!")
-
+        while in7 != 'y' and in7 != 'n':
+            in7 = input("Would you like a sandwhich?")
+            if in7 == 'y':
+                print("Your sandwhich options are,")
+                print(list(sandoptions.keys()))
+                in1 = input("What type of sandwhich would you like?")
+                in1 = in1.lower()
+                try:
+                    price = sandoptions[in1]
+                    br = False
+                except KeyError:
+                    print("That is not an option!")
+            elif in7 != 'n' and in7 != 'y':
+                print("That is not an option! Please input y or n.")
         print(price)
         while in2 != 'y' and in2 != 'n':
             in2 = input("Would you like a beverage? y/n")
@@ -39,7 +40,6 @@ class Funcs:
                 print(list(bevoptions.keys()))
                 in3 = input("What size beverage would you like?")
                 br1 = True
-
                 in3 = in3.lower()
                 try:
                     priceB = bevoptions[in3]
@@ -47,10 +47,8 @@ class Funcs:
                 except:
                     print("That is not an option!")
                     in2 = "Error"
-
             elif in2 != 'y' and in2 != 'n':
                 print("Please input y or n.")
-
         while in4 != 'y' and in4 != 'n':
             in4 = input("Would you like french fries?")
             if in4 == 'y':
@@ -59,17 +57,20 @@ class Funcs:
                 print(list(fryoptions.keys()))
                 in5 = input("What size would you like?")
                 in5 = in5.lower()
-                in6 = input("Would you like to mega size?")
-                if in6 == 'n':
-                    fryprice = fryoptions[in5]
-                else:
-                    fryprice = 2.00
+                in6 = ""
+                while in6 != 'y' and in6 != 'n':
+                    in6 = input("Would you like to mega size?")
+                    if in6 == 'n':
+                        fryprice = fryoptions[in5]
+                    elif in6 == 'y':
+                        fryprice = 2.00
+                    else:
+                        print('That is not an option!')
             elif in4 != 'y' and in4 != 'n':
                 print("Please input y or n.")
         numketc = input("How many ketchup packets would you like?")
         P_ket = int(numketc) * 0.25
         return price + priceB + fryprice + P_ket - 1 if in2 == 'y' and in4 == 'y' else price + priceB + fryprice + P_ket
-
 cont = True
 FinalVals = []
 while cont:
